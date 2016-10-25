98479632628f54991e8803440e2ca5bed5c277fa
Brad Baumel <Baumelbi@gmail.com>
2016-10-25 11:52:44 -0700

    updating

--- WeatherApp/app/src/main/java/com/example/baumelbi/weatherapp/WeatherActivity.java
+++ WeatherApp/app/src/main/java/com/example/baumelbi/weatherapp/WeatherActivity.java
@@ -2,18 +2,25 @@ package com.example.baumelbi.weatherapp;
 
 import android.app.ProgressDialog;
 import android.graphics.drawable.Drawable;
-import android.os.Bundle;
+import android.location.Address;
+import android.location.Geocoder;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.EditText;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 
 import com.example.baumelbi.weatherapp.data.Channel;
 import com.example.baumelbi.weatherapp.data.Item;
 import com.example.baumelbi.weatherapp.service.WeatherServiceCallback;
 import com.example.baumelbi.weatherapp.service.YahooWeatherService;
+
+import java.io.IOException;
+import java.util.List;
 
 public class WeatherActivity extends AppCompatActivity implements WeatherServiceCallback {
 
@@ -26,15 +33,16 @@ public class WeatherActivity extends AppCompatActivity implements WeatherService
     private ProgressDialog dialog;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
-        weatherIconImageView = (ImageView) findViewById(R.id.weatherIconImageView);
-        temperatureTextView = (TextView) findViewById(R.id.temperatureTextView);
-        conditionTextView = (TextView) findViewById(R.id.conditionTextView);
-        locationTextView = (TextView) findViewById(R.id.locationTextView);
+        weatherIconImageView = (ImageView)findViewById(R.id.weatherIconImageView);
+        temperatureTextView = (TextView)findViewById(R.id.temperatureTextView);
+        conditionTextView = (TextView)findViewById(R.id.conditionTextView);
+        locationTextView = (TextView)findViewById(R.id.locationTextView);
 
         service = new YahooWeatherService(this);
         dialog = new ProgressDialog(this);
@@ -43,15 +51,22 @@ public class WeatherActivity extends AppCompatActivity implements WeatherService
 
 
         service.refreshWeather("Seattle, WA");
-    }
 
-
-    public void onSearch(View view) {
-
-        EditText location_tf = (EditText)findViewById(R.id.locationEditText);
         String location = location_tf.getText().toString();
-        service.refreshWeather(location);
 
+        List<Address> addressList = null;
+        if (location != null || !location.equals("")) {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(location, 1);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+        }
     }
 
 
@@ -62,14 +77,14 @@ public class WeatherActivity extends AppCompatActivity implements WeatherService
 
 
         Item item = channel.getItem();
-        int resourceId = getResources().getIdentifier("drawable/icon_" + item.getCondition().getCode(), null, getPackageName());
+        int resourceId = getResources().getIdentifier("drawable/icon_"+ item.getCondition().getCode(), null, getPackageName());
 
         @SuppressWarnings("deprecation")
         Drawable weatherIconDrawable = getResources().getDrawable(resourceId);
 
         weatherIconImageView.setImageDrawable(weatherIconDrawable);
 
-        temperatureTextView.setText(item.getCondition().getTemperature() + "\u00B0" + channel.getUnits().getTemperature());
+        temperatureTextView.setText(item.getCondition().getTemperature()+"\u00B0"+channel.getUnits().getTemperature());
         conditionTextView.setText(item.getCondition().getDescription());
         locationTextView.setText(service.getLocation());
     }
